9bcc655ccec7ee6019fe597822f1906c5c8152ec
macbook <rushabhpicha12@gmail.com>
2022-04-17 11:17:26 -0700

    My second commit

--- app/src/main/java/com/example/countriesapp/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/example/countriesapp/viewmodel/MainActivityViewModel.java
@@ -1,2 +1,11 @@
-package com.example.countriesapp.viewmodel;public class MainActivityViewModel {
+package com.example.countriesapp.viewmodel;
+
+import android.widget.Toast;
+
+public class MainActivityViewModel {
+    String firstCommit = "Hello";
+    String secondCommit = "My Second commit";
+    String thirdCommit = "My third commit";
+
+
 }
